d194dd698fcc1aadc1af66ec9653e9e8aefd2417
Ridah Omran <ridahomran98@gmail.com>
2025-04-14 22:20:58 +0200

    Add project files.

---
+++ Presentation_WebApp/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    [Route("Sign-Up")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [Route("Sign-In")]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    public new IActionResult SignOut()
+    {
+        return RedirectToAction("Index", "Home");
+    }
+}
